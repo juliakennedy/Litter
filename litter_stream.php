57796992c007da88b80ddb51404ee6d563610f5b
2014-08-27 <cwhitte1@titan13.cs.trinity.edu>
2017-03-19 15:34:15 -0500

    MARCUS

---
+++ litter_stream.php
@@ -0,0 +1,60 @@
+<html>
+  <p> Welcome! </p>
+  <p> Here is some of today's kitty litter! </p>
+</html>
+
+<?php
+  session_start();
+  include("config.php");
+  $login = $_SESSION['loginname'];
+  //echo $login;
+  $sql = "SELECT ID FROM task5 WHERE username = '$login'";
+  //echo $sql;
+
+  //$sql = "SELECT username FROM task5";
+
+  $result = mysqli_query($link, $sql);
+  if(!$result) {
+    echo FAILEDQUERY;
+  }
+
+  $foundid = mysqli_fetch_assoc($result);
+  $idfound = $foundid['ID'];
+  /*
+  while($row = mysqli_fetch_assoc($result)) {
+    $idfound = $row['ID'];
+    //echo " ";
+  }
+  */
+  //echo $idfound;
+  $grab = "SELECT item FROM todolist WHERE userid = $idfound";
+  $items = mysqli_query($link, $grab);
+
+  $counter = 1;
+
+  while($row = mysqli_fetch_assoc($items)) {
+    echo $counter;
+    echo " ";
+    echo $row['item'];
+    echo nl2br("\n");
+    $counter = $counter + 1;
+    //echo " ";
+  }
+
+  $_SESSION['userid'] = $idfound;
+?>
+
+  <html>
+  <br>
+  <form action="additem.php"  method="post">
+    Add To-Do Item: <input type="text" name="newitem">
+    <input type="submit">
+    <br>
+  </form>
+  <form action="deleteitem.php" method="post">
+    Delete Item #: <input type="text" name="indextodelete">
+    <input type="submit">
+    <br>
+  </form>
+  <a href="logout.php">Log Out</a>
+</html>
