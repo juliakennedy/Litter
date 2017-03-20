facd1f5063b73751ea0d71c84c61ff718fe44275
2013-08-28 <jkenned1@titan14.cs.trinity.edu>
2017-03-19 19:08:11 -0500

    Load RSS entires into db on login page using ajax

--- litter_addRSSitems.php
+++ litter_addRSSitems.php
@@ -1,4 +1,26 @@
 <?php
+session_start();
+include("litter_config.php");
 
+$userid = -1;
+$item = $_POST['item'];
+$sig = "Hacker News";
+
+//echo $userid;
+//echo $item;
+
+if(!($item == "")) {
+  $check = "SELECT ID FROM litters WHERE litter_text = '$item'";
+  $results = mysqli_query($link, $check) or trigger_error(mysql_error. "in" .$sql);
+  if(!$results) {echo "BAD QUERY";}
+  $count = mysqli_num_rows($results);
+  if($count == 0) {
+    $sql = "INSERT INTO litters VALUES ($userid,'$sig',now(),'$item')";
+    //echo $sql;
+    $result = mysqli_query($link, $sql);
+
+  }
+
+}
 
  ?>
